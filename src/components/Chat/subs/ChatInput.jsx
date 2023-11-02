c702dad763f2d160dbb0c56c451bd2d0c513899e
moboluwade <folarinboluwade.com>
2023-11-02 14:20:28 +0100

    add border styles to chat input

--- src/components/Chat/subs/ChatInput.jsx
+++ src/components/Chat/subs/ChatInput.jsx
@@ -1,15 +1,19 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import SendIcon from '@mui/icons-material/Send';
+import { AttachFile } from '@mui/icons-material';
 
 const ChatInput = () => {
+    const [chatInput, setChatInput] = useState('')
     const inputRef = useRef()
-    
+
     return (
-        <div className='sticky bottom-0 bg-white py-2 px-10 w-full min-h-[3rem] flex flex-row items-center justify-between'>
-            <textarea ref={inputRef}  onInput={(inputRef)=>inputRef.currentTarget.style.height = inputRef.currentTarget.scrollHeight + 'px'}  className='w-full min-h-8 max-h-32 overflow-auto resize-none focus:outline-none' name="user-chat" id="user-chat-input"></textarea>
-            {/* <span>'Write a message to prove that the space between input and icon will not increase'</span> */}
-            <div className='pl-2'>
-                <SendIcon className='fill-blueGrey-500' />
+        <div className='sticky bottom-0 bg-white py-2 px-10 pl-5 w-full min-h-[3rem] flex flex-row items-end justify-between'>
+            <textarea placeholder='Write a message' ref={inputRef} onChange={(e) => { setChatInput(e.target.value) }} onInput={(inputRef) => { inputRef.currentTarget.style.height = 'auto'; inputRef.currentTarget.style.height = inputRef.currentTarget.scrollHeight + 'px' }} className='w-full min-h-8 max-h-32 pl-2 pt-2 overflow-auto resize-none border-2 border-blue-gray-600 rounded-md focus:outline-none scroll overscroll-contain' name="user-chat" id="user-chat-input"></textarea>
+            <div className=' cursor-pointer ml-2 rounded-md border-[1.5px] border-white hover:border-blue-gray-500 w-10 h-10 flex flex-row items-center justify-center'>
+                <AttachFile className=' fill-blue-gray-500' />
+            </div>
+            <div className=' cursor-pointer ml-2 rounded-md border-[1.5px] border-white hover:border-blue-gray-500 w-10 h-10 flex flex-row items-center justify-center'>
+                <SendIcon className='fill-blue-gray-500' />
             </div>
         </div>
     )
